e89ebe5794ba3cf68728c0f2ff58b72be95c0b1f
liuqiang <liu1142865396@gmail.com>
2021-01-05 17:44:17 +0800

    feat(Spring Gateway): 配置方式实现根据服务名负载均衡

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author liuqiang
+ * @since 2021-01-05
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        //2021-01-05T17:08:19.048116400+08:00[Asia/Shanghai]
+        System.out.println(ZonedDateTime.now());
+    }
+}
