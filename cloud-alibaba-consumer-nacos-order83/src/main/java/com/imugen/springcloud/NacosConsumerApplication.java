90438717c7369a47b95194a2fa9cfa8490488473
liuqiang <liu1142865396@gmail.com>
2021-09-12 18:26:07 +0800

    update nacos code

--- cloud-alibaba-consumer-nacos-order83/src/main/java/com/imugen/springcloud/NacosConsumerApplication.java
+++ cloud-alibaba-consumer-nacos-order83/src/main/java/com/imugen/springcloud/NacosConsumerApplication.java
@@ -1,5 +1,6 @@
 package com.imugen.springcloud;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -7,10 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -32,19 +30,20 @@ public class NacosConsumerApplication {
     }
 
     @RestController
+    @RequiredArgsConstructor(onConstructor_ = {@Autowired})
     public class TestController {
-
         private final RestTemplate restTemplate;
 
         @Value("${service-url.nacos-user-service}")
         private String url;
 
-        @Autowired
-        public TestController(RestTemplate restTemplate) {
-            this.restTemplate = restTemplate;
-        }
+//        @Autowired
+//        public TestController(RestTemplate restTemplate) {
+//            this.restTemplate = restTemplate;
+//        }
 
-        @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+//        @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+        @GetMapping("/echo/{str}")
         public String echo(@PathVariable String str) {
 //            return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
             return restTemplate.getForObject(url + str, String.class);
